a8ee79e0d56c86330d7c1d7c71820db324c1d86f
Syed Shameer Sarwar <syedshameersarwar@gmail.com>
2021-01-18 18:27:29 +0500

    test commit #6

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,6 @@ use warp::Filter;
 
 #[tokio::main]
 async fn main() {
-    let route = warp::any().map(|| "Hi!!! i am only for testing argo stack!");
+    let route = warp::any().map(|| "Hi!!! i am only for testing argo ambassador stack!");
     warp::serve(route).run(([0, 0, 0, 0], 3030)).await;
 }
