4b95e8f4e4252b0021da5e5bba393064597c7d03
Syed Shameer Sarwar <syedshameersarwar@gmail.com>
2021-01-20 03:48:27 +0500

    test commit #18

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,6 @@ use warp::Filter;
 
 #[tokio::main]
 async fn main() {
-    let route = warp::any().map(|| "Hiiiiii!!!!!! I am testing k8s argo ambassador stack on linode with succcess finally!!!!");
+    let route = warp::any().map(|| "Hiiiiii!!!!!! I am testing k8s argo ambassador stack on linode with succcess finally!!!!!!!");
     warp::serve(route).run(([0, 0, 0, 0], 3030)).await;
 }
